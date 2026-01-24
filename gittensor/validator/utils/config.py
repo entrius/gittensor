6457ebe09cd4095a793ea331d071a13b1d4cc237
anderdc <alexanderdcastaneda@gmail.com>
2026-01-24 14:15:40 -0600

    move around some env variables

--- gittensor/validator/utils/config.py
+++ gittensor/validator/utils/config.py
@@ -4,7 +4,6 @@ import bittensor as bt
 
 from gittensor import __version__
 
-
 VALIDATOR_WAIT = 60  # 60 seconds
 VALIDATOR_STEPS_INTERVAL = 120  # 2 hours, every time a scoring round happens
 PR_LOOKBACK_DAYS = 90  # how many days a merged pr will count for scoring
@@ -12,6 +11,10 @@ PR_LOOKBACK_DAYS = 90  # how many days a merged pr will count for scoring
 # required env vars
 WANDB_API_KEY = os.getenv('WANDB_API_KEY')
 WANDB_PROJECT = os.getenv('WANDB_PROJECT', 'gittensor-validators')
+WANDB_VALIDATOR_NAME = os.getenv('WANDB_VALIDATOR_NAME', 'vali')
+
+# optional env vars
+STORE_DB_RESULTS = os.getenv('STORE_DB_RESULTS', 'false').lower() == 'true'
 
 # log values
 bt.logging.info(f'VALIDATOR_WAIT: {VALIDATOR_WAIT}')

098c17f9fada08b168f9b02e5f3a3b5eb77002d7
dataCenter430 <titan032000@gmail.com>
2026-04-16 00:08:17 +0200

    refactor: deduplicate get_contract_address into shared utils

--- gittensor/utils/utils.py
+++ gittensor/utils/utils.py
@@ -2,9 +2,17 @@
 GitTensor Utilities
 """
 
+import os
 from typing import Dict
 
 
 def parse_repo_name(repo_data: Dict):
     """Normalizes and converts repository name from dict"""
     return f'{repo_data["owner"]["login"]}/{repo_data["name"]}'.lower()
+
+
+def get_contract_address() -> str:
+    """Get contract address. Override via CONTRACT_ADDRESS env var for dev/testing."""
+    from gittensor.constants import CONTRACT_ADDRESS
+
+    return os.environ.get('CONTRACT_ADDRESS') or CONTRACT_ADDRESS
